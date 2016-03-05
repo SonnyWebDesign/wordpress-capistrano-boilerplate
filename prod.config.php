865f6591d9b091fbd9267dea83b7db3cb6e40d21
Andrea Sonny <andreasonny83@gmail.com>
2016-03-05 16:42:31 +0000

    Prepare 0.1.2 release

--- prod.config.php
+++ prod.config.php
@@ -22,8 +22,20 @@ define('DB_HOST', 'localhost');
  define('LOGGED_IN_SALT',   'put your unique phrase here');
  define('NONCE_SALT',       'put your unique phrase here');
 
-define('WP_DEBUG', false);
-define('WP_DEBUG_DISPLAY', false);
-define('WP_DEBUG_LOG', true);
+// Hides errors from being displayed on-screen
+@ini_set( 'display_errors', false );
+// Store all the errors inside the default server error.log if no `error_log` defined
 @ini_set( 'log_errors', true );
-@ini_set('display_errors', false);
+// Define where you want to store the error log file on your server side
+// @ini_set( 'error_reporting', E_ALL );
+// @ini_set('error_log', dirname(__FILE__) . '/php-errors.log');
+// Turns WordPress debugging off
+define( 'WP_DEBUG', false );
+// Don't display errors on screen
+define( 'WP_DEBUG_DISPLAY', false );
+// Tells WordPress not to log everything to the /wp-content/debug.log
+define( 'WP_DEBUG_LOG', false );
+// Disable the editing of theme and plugin files
+// define( 'DISALLOW_FILE_EDIT', true );
+// Disable installing new themes and plugins, and updating them
+// define( 'DISALLOW_FILE_MODS', true );
